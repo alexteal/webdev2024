f108d4f1ee631763533e15e7a8b9fb624523d836
alexteal <29980694+alexteal@users.noreply.github.com>
2024-04-08 20:17:35 -0400

    no more cookies

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,25 @@
+"use client";
 import Link from "next/link";
 import styles from "./page.module.css";
+import { useState } from "react";
 //primary routing
 export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
   return (
     <main className={styles.main}>
       <h1>Hello</h1>
+      {isLoggedIn ? "true" : "false"}
       <button className="button">
         <Link href="/">Main</Link>
       </button>
       <button className="button">
         <Link href="/Login">Login</Link>
+      </button>
+      <button className="button" onClick={logoutHandler}>
+        <p>Logout</p>
       </button>
       <button className="button">
         <Link href="/CreateAccount">Signup</Link>
